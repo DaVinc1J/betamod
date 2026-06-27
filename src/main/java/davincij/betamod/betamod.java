d61a20683131b47dc7a96d8627a192453d4505f4
DaVinc1J <tristanlowe48@gmail.com>
2026-06-27 13:48:53 +0930

    init

---
+++ src/main/java/davincij/betamod/betamod.java
@@ -0,0 +1,23 @@
+package davincij.betamod.events.init;
+
+import davincij.betamod.betamoditems;
+// + imports for Namespace, TemplateItem, Item, ItemRegistryEvent, EventListener, Entrypoint
+//   (let your IDE add these)
+
+public class ItemListener {
+        @Entrypoint.Namespace
+        public static Namespace NAMESPACE;
+
+        @EventListener
+        public void registerItems(ItemRegistryEvent event) {
+                BetaModItems.greatwoodStick  = simpleItem("greatwood_stick");
+                BetaModItems.silverwoodStick = simpleItem("silverwood_stick");
+                BetaModItems.ironCap         = simpleItem("iron_cap");
+                BetaModItems.silverCap       = simpleItem("silver_cap");
+                BetaModItems.thaumiumCap     = simpleItem("thaumium_cap");
+        }
+
+        private static Item simpleItem(String name) {
+                return new TemplateItem(NAMESPACE.id(name)).setTranslationKey(NAMESPACE, name);
+        }
+}
